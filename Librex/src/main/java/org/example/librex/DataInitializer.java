76ba7a10e29c07af0bf9b4b1aab4bf4afcebd76e
Zanovadry <marcel.pokrywka@gmail.com>
2025-12-04 19:24:39 +0100

    Temporary H2 database

---
+++ Librex/src/main/java/org/example/librex/DataInitializer.java
@@ -0,0 +1,51 @@
+package org.example.librex;
+
+import jakarta.annotation.PostConstruct;
+import org.example.librex.dictionaries.countries.Country;
+import org.example.librex.dictionaries.permissions.Role;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+//Temporary class for populating temporary H2 database for test purposes
+//TODO: Delete after linking postgres
+
+@Component
+public class DataInitializer {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public DataInitializer(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostConstruct
+    public void init() {
+
+        System.out.println("Initializing permissions...");
+
+        jdbcTemplate.execute("DELETE FROM permissions");
+        jdbcTemplate.execute("""
+                            INSERT INTO permissions (ROLE) VALUES 
+                            ('%s'), ('%s'), ('%s')
+                            """.formatted(
+                        Role.ADMIN,
+                        Role.CUSTOMER,
+                        Role.LIBRARIAN
+        ));
+
+        System.out.println("Initializing countries...");
+
+        jdbcTemplate.execute("DELETE FROM countries");
+        jdbcTemplate.execute("""
+                            INSERT INTO countries (COUNTRY_NAME) VALUES 
+                            ('%s'), ('%s'), ('%s')
+                            """.formatted(
+                Country.POLAND,
+                Country.FINLAND,
+                Country.UNITED_STATES
+        ));
+    }
+
+
+}
+
