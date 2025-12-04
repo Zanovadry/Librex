84bdd30660be11366387cbec176233182d62c071
Zanovadry <marcel.pokrywka@gmail.com>
2025-12-04 17:06:23 +0100

    Spring Boot Init

---
+++ Librex/src/main/java/org/example/librex/LibrexApplication.java
@@ -0,0 +1,13 @@
+package org.example.librex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrexApplication.class, args);
+    }
+
+}
---
+++ Librex/src/test/java/org/example/librex/LibrexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.librex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
